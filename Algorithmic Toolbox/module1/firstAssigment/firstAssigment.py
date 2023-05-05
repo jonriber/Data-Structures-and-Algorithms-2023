1d26d7f5d29b68b6eee9654db3a80a784f03b084
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-05 22:54:21 +0100

    adjustment according to coursera submition format

--- Algorithmic Toolbox/module1/firstAssigment/firstAssigment.py
+++ Algorithmic Toolbox/module1/firstAssigment/firstAssigment.py
@@ -1,2 +1,10 @@
-def sumTwoNumbers(a,b): 
-    return print(a+b)
+import sys
+input = sys.stdin.read()
+parts = input.split()
+input1 = int(parts[0])
+input2 = int(parts[1])
+print(input1+input2)
+
+# def sumTwoNumbers(input1,input2): 
+#     return print(a+b)
+
