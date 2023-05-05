ec027a183c762600e612eab7dfa89badff51199f
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-05 22:57:11 +0100

    alternative way with function declaration

--- Algorithmic Toolbox/module1/firstAssigment/firstAssigment.py
+++ Algorithmic Toolbox/module1/firstAssigment/firstAssigment.py
@@ -3,8 +3,9 @@ input = sys.stdin.read()
 parts = input.split()
 input1 = int(parts[0])
 input2 = int(parts[1])
-print(input1+input2)
+# print(input1+input2)
 
-# def sumTwoNumbers(input1,input2): 
-#     return print(a+b)
+def sumTwoNumbers(a,b): 
+    return print(a+b)
 
+sumTwoNumbers(input1,input2)
