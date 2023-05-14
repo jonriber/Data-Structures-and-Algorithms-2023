2d838d68c3833e95ff4d010b2f6a74f05aad4461
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-14 23:30:23 +0100

    most pythonic solution

--- Algorithmic Toolbox/module1/maximum_pairwise/maximumPairwise.py
+++ Algorithmic Toolbox/module1/maximum_pairwise/maximumPairwise.py
@@ -1,3 +1,12 @@
-import sys
-input = sys.stdin.read()
-inputNumbers = input.split()
+def max_pairwise_product(numbers): 
+#    print("number sorted:",numbers)
+   reversedList = sorted(numbers,reverse=True)
+#    print(reversedList)
+#    print(reversedList[0],reversedList[1])
+   return (reversedList[0]*reversedList[1])
+
+
+if __name__ == '__main__':
+    _ = int(input())
+    input_numbers = list(map(int, input().split()))
+    print(max_pairwise_product(input_numbers))
