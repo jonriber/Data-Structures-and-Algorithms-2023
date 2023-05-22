1d187f2ee4585fb611aba111e3246fe8042f965d
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-22 14:33:31 +0100

    testing cases

--- Algorithmic Toolbox/module 2/fibonacci algorithm/naive_alternative_solution_1.py
+++ Algorithmic Toolbox/module 2/fibonacci algorithm/naive_alternative_solution_1.py
@@ -1,12 +1,20 @@
-import sys
-input = sys.stdin.read()
-fib_number = int(input)
+# import sys
+# input = sys.stdin.read()
+
+# fib_number = int(input)
 
 ## Recursive implementation
 # worst case scenario, time related
 def fibonacci(n):
+    print("n:",n)
     if(n<=1):
+        print("case 1")
         return 1
     else:
+        print("returning the combination")
         return fibonacci(n-1) + fibonacci(n-2)
+    
+if __name__ == "__main__":
+    result = fibonacci(8)
+    print("resultado é:",result)
     
