d84feafa0215cfa4647bf57ad4a7b202f7e33b1c
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-26 12:06:19 +0100

    fibonacci best option

--- Algorithmic Toolbox/module 2/fibonacci algorithm/alternative_solution_2.py
+++ Algorithmic Toolbox/module 2/fibonacci algorithm/alternative_solution_2.py
@@ -1,6 +1,13 @@
 def fibo(n):
-    return n
+    fibo_array = [i for i in range(0,n+1)]
+    print('array:',fibo_array)
+
+    for num in range(2,n):
+        print(fibo_array[num])
+        fibo_array[num] = fibo_array[num-1] + fibo_array[num-2]
+    print("final array:",fibo_array)
+    return fibo_array[n-1]
 
 if __name__ == "__main__":
-    result =fibo()
+    result = fibo(10)
     print("result is:",result)
