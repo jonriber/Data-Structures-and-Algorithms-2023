7f93cd76014462a5f69fa6f74d1d94dc59d0ccce
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-26 15:19:08 +0100

    CLEANING fibo alternative to submit

--- Algorithmic Toolbox/module 2/fibonacci algorithm/alternative_solution_2.py
+++ Algorithmic Toolbox/module 2/fibonacci algorithm/alternative_solution_2.py
@@ -1,13 +1,12 @@
 def fibo(n):
     fibo_array = [i for i in range(0,n+1)]
-    print('array:',fibo_array)
 
     for num in range(2,n):
-        print(fibo_array[num])
+        # print(fibo_array[num])
         fibo_array[num] = fibo_array[num-1] + fibo_array[num-2]
-    print("final array:",fibo_array)
+    # print("final array:",fibo_array)
     return fibo_array[n-1]
 
 if __name__ == "__main__":
     result = fibo(10)
-    print("result is:",result)
+    # print("result is:",result)
