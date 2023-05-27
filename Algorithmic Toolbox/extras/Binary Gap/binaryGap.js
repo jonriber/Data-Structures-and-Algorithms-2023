58c93638cd2addd07a05f8e5dbe38b93964973cf
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-27 11:27:36 +0100

    javascript version of binary gap

---
+++ Algorithmic Toolbox/extras/Binary Gap/binaryGap.js
@@ -0,0 +1,22 @@
+
+
+function binaryGap(num){
+    if(num <=0) return 0
+    let 
+        binaryString = num.toString(2),//transform decimal number into binary
+        maxGapValue = 0
+        gapCounter = 0
+    console.log("binaryString:",binaryString)
+    for(let i in binaryGap){
+        if(i == "0") gapCounter +=1
+        else if(i =="1"){
+            maxGapValue = Math.max(maxGapValue,gapCounter)
+            gapCounter =0
+        }
+    }
+    return maxGapValue
+}
+
+const testingCases = [1,4,6,12,45,7,15,529,32,20,9]
+
+testingCases.forEach(item => console.log(binaryGap(item)))
