82c3738f41d75eeeffc0328b8b4fc051ca4bc158
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-31 10:49:14 +0100

    pair of cars

--- Algorithmic Toolbox/extras/Passing Cars/passingCars.js
+++ Algorithmic Toolbox/extras/Passing Cars/passingCars.js
@@ -2,8 +2,19 @@
 
 function passingCars(array){
     if(array.length <=1) return 0
-
-    
+    let 
+        carsGoingWest = 0,
+        pairOfPassingCars = 0
+    //tartet is one iteration only ----O(n)
+    for( let i in array){
+        console.log("i:",i)
+        if(i == 0){//check if its zero
+            carsGoingWest +=1
+        }else{ //if its not zero, it gotta be one
+            pairOfPassingCars = carsGoingWest +1
+        }
+    }
+    return pairOfPassingCars //this is a number, total os cars passing through
 }
 
 const testingCases = [[0,1,0,1,1]]
