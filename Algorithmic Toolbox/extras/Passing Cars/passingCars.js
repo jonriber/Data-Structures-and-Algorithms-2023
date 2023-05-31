df505dcde59a241f53b28d69edd4f69ce5c728e9
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-31 11:11:29 +0100

    array reading correction

--- Algorithmic Toolbox/extras/Passing Cars/passingCars.js
+++ Algorithmic Toolbox/extras/Passing Cars/passingCars.js
@@ -8,15 +8,16 @@ function passingCars(array){
     //tartet is one iteration only ----O(n)
     for( let i in array){
         console.log("i:",i)
-        if(i == 0){//check if its zero
+        if(arra[i] == 0){//check if its zero
             carsGoingWest +=1
         }else{ //if its not zero, it gotta be one
             pairOfPassingCars += carsGoingWest
+            if(pairOfPassingCars > 1000000000) return -1
         }
     }
     return pairOfPassingCars //this is a number, total os cars passing through
 }
 
-const testingCases = [[0,1,0,1,1]]
+const testingCases = [[0,1,0,1,1],[],[1],[0],[0,0,0,1,1]]
 
 testingCases.forEach(item => console.log(passingCars(item)))
