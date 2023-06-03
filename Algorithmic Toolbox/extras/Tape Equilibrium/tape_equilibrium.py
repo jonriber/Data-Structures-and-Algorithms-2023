2fd93c9e1aa79e88d3ee781d8ee03ead5db22c88
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-06-03 20:00:00 +0100

    tape_equilibrium python version

---
+++ Algorithmic Toolbox/extras/Tape Equilibrium/tape_equilibrium.py
@@ -0,0 +1,8 @@
+def tape_equilibrium(array):
+    totalSum = sum(array)
+
+if __name__ == "__main__":
+    testing_cases = [[3,1,2,4,3]]
+
+    for test in testing_cases:
+        print(tape_equilibrium(test))
