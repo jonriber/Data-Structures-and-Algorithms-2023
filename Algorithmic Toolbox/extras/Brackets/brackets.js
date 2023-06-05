66264572140e42dfa3b51d14cb7f7b178e10a567
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-06-05 14:23:31 +0100

    Brackets challenge init

---
+++ Algorithmic Toolbox/extras/Brackets/brackets.js
@@ -0,0 +1,10 @@
+//PESEUDO - CODE
+
+
+function brackets(str){
+    return 0 //not properly nested
+}
+
+const testingCases = ["{[()()]}","([)()]"]
+
+testingCases.forEach(item => console.log(brackets(item)))
