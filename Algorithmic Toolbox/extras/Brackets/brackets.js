ac4ed27595fbfe3954ac3375aa55c6a0397084b8
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-06-05 17:46:29 +0100

    fixing switch statements

--- Algorithmic Toolbox/extras/Brackets/brackets.js
+++ Algorithmic Toolbox/extras/Brackets/brackets.js
@@ -1,7 +1,53 @@
 //PESEUDO - CODE
+//CREATE A STACK, LAST IN --> FIRST OUT
+//IT IS AN ARRAY
 
+//STEP ONE: ITERATE OVER EACH ELEMENT IN ARRAY, AND CHECK IT´S TYPE, APPEND IN STACK ACCORDING TO ITS CODE
 
 function brackets(str){
+    let stack = [];
+
+    for(let i of str){ //of element gives us the array value, not index 
+        console.log(i)
+        if(stack.length<1){
+            if(i == ")" || i =="]" || i =="}") return 0 //if array is starting and first element is already wrong, simplky return 0
+            if( i =="{") stack.push(1)
+            else if(i =="[") stack.push(2)
+            else stack.push(3)
+        }else{
+            console.log("switch case",i)
+            switch(i){
+                case "{":
+                    console.log("{ aqui")
+                    break
+                case "[":
+                    console.log("[ aqui")
+
+                    break
+                case "(":
+                    console.log("( aqui")
+                    break
+                case ")":
+                    break
+                case "]":
+                    break
+                case "}":
+                    break
+                default:
+                    console.log("default case")
+                    return 0
+                    
+            }
+            // if(i == "{") stack.push(1)
+            // else if(i == "[") stack.push(2)
+            // else if(i == "(") stack.push(3)
+            // else if(i == ")" && stack[stack.length-1] != 3) stack.push(-3)
+            // else if(i =="]") stack.push(-2)
+            // else if(i == "}") stack.push(-3)
+        }
+        
+    }
+    console.log("stack:",stack)
     return 0 //not properly nested
 }
 
