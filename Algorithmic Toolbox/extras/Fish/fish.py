3aa6daf92c30c168407429c739208c221cf5b27e
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-06-17 19:14:18 +0100

    implementation with stack and vectors

--- Algorithmic Toolbox/extras/Fish/fish.py
+++ Algorithmic Toolbox/extras/Fish/fish.py
@@ -1,6 +1,27 @@
+#PSEUDO CODE NOTATION
+#Objective is to analyse fish going in differnt direction
+#Check their sizes when they cross each other
+#Inser the remaining fish in a stack
 def fish(fish_sizes, fish_directions):
     print("function started",fish_sizes,fish_directions)
+    eaten=0
+    ds = []
     
+    for i in range(0,len(fish_directions)):
+        print(i)
+        print("fish size:",fish_sizes[i])
+        if fish_directions[i] == 1:
+            ds.append(fish_sizes[i])
+        else:
+            while len(ds) != 0:
+                if ds[-1] > fish_sizes[i]:
+                    eaten+=1
+                    break
+                elif ds[-1] < fish_sizes[i]:
+                    ds.pop()
+                    eaten+=1
+    return (len(fish_sizes) - eaten)
+
 
 if __name__ == "__main__":
     testing_cases = [
