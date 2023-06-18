d60c6905e0ad0bcb45b63d4aa96ab6188a66b648
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-06-18 23:36:24 +0100

    stone wall statement

---
+++ Algorithmic Toolbox/extras/stoneWall/stone_wall.py
@@ -0,0 +1,10 @@
+def stone_wall(stone_array):
+    print(stone_array)
+    
+    pass
+
+if __name__ == "__main__":
+    testing_cases= [[8,8,5,7,9,8,7,4,8],]
+
+    for wall in testing_cases:
+        print(stone_wall(wall))
